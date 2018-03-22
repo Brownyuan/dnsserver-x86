6e7afe8d8381b88fe3c6439dbfaadb3dacf7bcb3
peter.pan <peter.pan@gnomito.com>
2018-03-22 17:10:24 +0800

    add control to control project

---
+++ dnsserver-control.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+project=`cd $(dirname $0);pwd`
+working_dir="/etc/dnsserver"
+tamplate="$project/tamplate"
+
+Usage() {
+    echo "Usage: $0 { install | uninstall | restart | status }"
+    exit 1
+}
+
+Parameter_judge() {
+    correct_num="$1"
+    real_num="$2"
+    if [ "$correct_num" != "$real_num" ];then
+        Usage
+    fi
+}
+
+Config() {
+    wan="$1"
+    dns_port="$2"
+
+    sudo mkdir -p $working_dir
+
+    cp $tamplate/dns-port-forward.tamplate dns-port-forward
+    sed -i "s/interface/$wan/g" dns-port-forward
+    sudo mv dns-port-forward $working_dir/
+
+    cp $tamplate/dns.service.tamplate dns.service
+    sed -i "s/PORT/$dns_port/g" dns.service
+    sudo mv dns.service /etc/systemd/system/
+}
+
+Restore() {
+    sudo rm -r $working_dir
+    sudo rm /etc/systemd/system/dns.service
+}
+
+Start() {
+    for cmd in start enable status
+    do
+        sudo systemctl $cmd dns.service
+    done
+}
+
+Stop() {
+    for cmd in stop disable status
+    do 
+        sudo systemctl $cmd dns.service
+    done
+}
+
+Status() {
+    sudo iptables -t nat -L PREROUTING -nv
+    sudo iptables -t nat -L POSTROUTING -nv
+}
+
+case "$1" in 
+    install)
+        Parameter_judge 3 $# 
+        Config $2 $3
+        Start 
+        ;;
+
+    uninstall)
+        Parameter_judge 1 $#
+        Stop
+        Restore
+        ;;
+
+    restart)
+        Parameter_judge 1 $#
+        Stop
+        Start
+        ;;
+
+    status)
+        Parameter_judge 1 $#
+        Status
+        ;;
+
+    *)
+        Usage
+        ;;
+esac
